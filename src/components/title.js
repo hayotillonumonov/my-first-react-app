d60859c7757a333c2e9f51d86fa410bba11da3f7
Hayotillo <hayotnumon@gmail.com>
2023-07-30 16:37:27 +0500

    015. Mutiple Component

---
+++ src/components/title.js
@@ -0,0 +1,10 @@
+const title = ()=>{
+    return (
+        <div>
+            <h1 className="title">Hayotillo's Kingdim Events</h1>
+            <p className="subtitle">All events will be here</p>
+        </div>
+    )
+}
+
+export default title
