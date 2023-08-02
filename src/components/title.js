6f20a59bb09f7ed0039ce12d052da208bcfc1588
Hayotillo <hayotnumon@gmail.com>
2023-08-02 20:58:13 +0500

    022. Global va Component Styles

--- src/components/title.js
+++ src/components/title.js
@@ -1,10 +1,11 @@
+import './title.css'
 const Title = ({title, subtitle})=>{
     return (
-        <>
+        <div className='title-block'>
             <h1 className="title">{title}</h1>
             <br/>
             <p className="subtitle">{subtitle}</p>
-        </>
+        </div>
     )
 }
 
