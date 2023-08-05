06954b5094155c0b8a1590fa2abced4e65dda1f6
Hayotillo <hayotnumon@gmail.com>
2023-08-05 19:12:39 +0500

    029. Reset input

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,20 +1,28 @@
-// style
+// Style
 import "./NewEventForm.css";
+
 import { useState } from "react";
 
 function NewEventForm() {
   const [title, settitle] = useState("");
   const [date, setdate] = useState("");
+  const resetInputs = ()=>{
+    settitle('')
+    setdate('')
+  }
   return (
     <form className="new-event-form">
       <label>
         <span>Event title:</span>
-        <input type="text" onChange={(e) => settitle(e.target.value)} />
+        <input type="text" onChange={(e) => settitle(e.target.value)} value={title} />
       </label>
       <label>
         <span>Event Date:</span>
-        <input type="date" onChange={(e) => setdate(e.target.value)} />
+        <input type="date" onChange={(e) => setdate(e.target.value)} value={date}/>
       </label>
+      <button type="button" onClick={resetInputs}>Reset inputs</button>
+      <br />
+      <br />
       <button>Submit</button>
       <hr />
       <p>Title: {title}</p>
